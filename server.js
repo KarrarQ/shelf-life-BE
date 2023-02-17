19ab1be798beb0d76259fb14c50f544df984b654
Karrar Qasim <Karawr1996@gmail.com>
2023-02-16 20:14:47 -0700

    Added initial logic for express server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const { response } = require('express');
 const express = require('express');
+const ourBooks = require('./data/books-data')
 const app = express();
 
 app.use(express.json())
@@ -8,11 +8,30 @@ app.use(express.static('public'))
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Shelf Life';
+app.locals.books = ourBooks;
 
 app.get('/', (request, response) => {
     response.send('This is for the books yo.')
-})
+});
 
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`)
+});
 
+app.get('/api/v1/books', (request, response) => {
+    const { books } = app.locals
+    response.status(200).json({ books })
+});
 
+app.get('/api/v1/books/:id', (request, response) => {
+    const { id } = request.params;
+    const book = app.locals.books.find(book => book.id === id);
+    if(!book) {
+        return response.status(404).json({
+            message:`no novel found with id of # ${id}`
+        })
+    } else {
+        response.status(200).json(book)
+    }
+});
 
