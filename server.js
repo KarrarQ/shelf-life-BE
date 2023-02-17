d7712f697c062f9cab679407b36a0802aed596db
Karrar Qasim <Karawr1996@gmail.com>
2023-02-16 20:58:43 -0700

    Added post for favorites list

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+const { request, response } = require('express');
 const express = require('express');
 const ourBooks = require('./data/books-data')
+const favorites = require('./data/favorites-data')
 const app = express();
 
-app.use(express.json())
-app.use(express.static('public')) 
+app.use(express.json());
+app.use(express.static('public'));
 //the line above refers to the 'PUBLIC' file
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Shelf Life';
 app.locals.books = ourBooks;
+app.locals.favorites = favorites;
 
 app.get('/', (request, response) => {
     response.send('This is for the books yo.')
@@ -35,3 +38,18 @@ app.get('/api/v1/books/:id', (request, response) => {
     }
 });
 
+
+app.post('/api/vi/favorites', (request, response) => {
+    const favorite = request.body;
+    const { id, title, description, amazon_link, author, recommendedBy, image } = favorite;
+    for(let requiredParameter of ['id', 'title', 'description', 'amazon_link', 'author', 'recommendedBy', 'image']) {
+        if(!favorite[requiredParameter]) {
+            response.status(422)
+            .send({
+                error: `Expected format: {id:<String>, title: <String>, description: <String>, amazon_link: <String>, author: <String>, recommendedBy: <String>, image: <String>}. You're missing a "${requiredParameter}" property.`
+            });
+        }
+    }
+    app.locals.favorites.push({ id, title, description, amazon_link, author, recommendedBy, image });
+    response.status(201).json({ id, title, description, amazon_link, author, recommendedBy, image });
+});
