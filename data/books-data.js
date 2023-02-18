d91c5a082d067cd71c85c2a2c01e8c1ac0991189
Reid Poole <poole.reid@gmail.com>
2023-02-18 15:13:38 -0600

    Change Key of id to isbn

--- data/books-data.js
+++ data/books-data.js
@@ -1,93 +1,114 @@
 const books = [
-    {
-      id: '9781250278210',
-      title: 'ABANDONED IN DEATH',
-      description: 'The 54th book of the In Death series. Eve Dallas investigates a homicide and the disappearance of other women who resemble that victim.',
-      amazon_link: 'https://www.amazon.com/dp/125027821X?tag=NYTBSREV-20',
-      author: 'J.D. Robb',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9781250278210.jpg'
-    },
-    {
-      id: '9780525559474',
-      title: 'THE MIDNIGHT LIBRARY',
-      description: 'Nora Seed finds a library beyond the edge of the universe that contains books with multiple possibilities of the lives one could have lived.',
-      amazon_link: 'https://www.amazon.com/dp/0525559477?tag=NYTBSREV-20',
-      author: 'Matt Haig',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9780525559474.jpg'
-    },
-    {
-      id: '9781501171345',
-      title: 'THE LAST THING HE TOLD ME',
-      description: 'Hannah Hall discovers truths about her missing husband and bonds with his daughter from a previous relationship.',
-      amazon_link: 'https://www.amazon.com/dp/1501171348?tag=NYTBSREV-20',
-      author: 'Laura Dave',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9781501171345.jpg'
-    },
-    {
-      id: '9780735222359',
-      title: 'THE LINCOLN HIGHWAY',
-      description: 'Two friends who escaped from a juvenile work farm take Emmett Watson on an unexpected journey to New York City in 1954.',
-      amazon_link: 'https://www.amazon.com/dp/0735222355?tag=NYTBSREV-20',
-      author: 'Amor Towles',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9780735222359.jpg'
-    },
-    {
-      id: '9780593356159',
-      title: 'THE MAID',
-      description: 'When a wealthy man is found dead in his room, a maid at the Regency Grand Hotel becomes a lead suspect.',
-      amazon_link: 'https://www.amazon.com/dp/0593356152?tag=NYTBSREV-20',
-      author: 'Nita Prose',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9780593356159.jpg'
-    },
-    {
-      id: '9781250274618',
-      title: 'THE CHRISTIE AFFAIR',
-      description: 'Miss Nan O’Dea becomes the mistress of Agatha Christie’s husband.',
-      amazon_link: 'https://www.amazon.com/dp/1250274613?tag=NYTBSREV-20',
-      author: 'Nina de Gramont',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9781250274618.jpg'
-    },
-    {
-      id: '9780525618584',
-      title: 'CITY OF THE DEAD',
-      description: 'The 37th book in the Alex Delaware series. Delaware and Sturgis investigate a double homicide.',
-      amazon_link: 'https://www.amazon.com/dp/0525618589?tag=NYTBSREV-20',
-      author: 'Jonathan Kellerman',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9780525618584.jpg'
-    },
-    {
-      id: '9780385546027',
-      title: "THE JUDGE'S LIST",
-      description: 'The second book in the Whistler series. Investigator Lacy Stoltz goes after a serial killer and closes in on a sitting judge.',
-      amazon_link: 'https://www.amazon.com/dp/0385546025?tag=NYTBSREV-20',
-      author: 'John Grisham',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9780385546027.jpg'
-    },
-    {
-      id: '9780316499774',
-      title: 'THE HORSEWOMAN',
-      description: 'As the Paris Olympics draw near, a mother and daughter, who are champion horse riders, compete against each other.',
-      amazon_link: 'https://www.amazon.com/dp/0316499773?tag=NYTBSREV-20',
-      author: 'James Patterson and Mike Lupica',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9780316499774.jpg'
-    },
-    {
-      id: '9781984818416',
-      title: 'WISH YOU WERE HERE',
-      description: 'Diana O’Toole re-evaluates her seemingly perfect life when a pandemic disrupts her vacation in the Galápagos Islands.',
-      amazon_link: 'https://www.amazon.com/dp/1984818414?tag=NYTBSREV-20',
-      author: 'Jodi Picoult',
-      recommendedBy: "Karrar Qasim",
-      image: 'https://storage.googleapis.com/du-prd/books/images/9781984818416.jpg'
-    }]
+  {
+    isbn: "9781250278210",
+    title: "ABANDONED IN DEATH",
+    description:
+      "The 54th book of the In Death series. Eve Dallas investigates a homicide and the disappearance of other women who resemble that victim.",
+    amazon_link: "https://www.amazon.com/dp/125027821X?tag=NYTBSREV-20",
+    author: "J.D. Robb",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9781250278210.jpg",
+  },
+  {
+    isbn: "9780525559474",
+    title: "THE MIDNIGHT LIBRARY",
+    description:
+      "Nora Seed finds a library beyond the edge of the universe that contains books with multiple possibilities of the lives one could have lived.",
+    amazon_link: "https://www.amazon.com/dp/0525559477?tag=NYTBSREV-20",
+    author: "Matt Haig",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780525559474.jpg",
+  },
+  {
+    isbn: "9781501171345",
+    title: "THE LAST THING HE TOLD ME",
+    description:
+      "Hannah Hall discovers truths about her missing husband and bonds with his daughter from a previous relationship.",
+    amazon_link: "https://www.amazon.com/dp/1501171348?tag=NYTBSREV-20",
+    author: "Laura Dave",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9781501171345.jpg",
+  },
+  {
+    isbn: "9780735222359",
+    title: "THE LINCOLN HIGHWAY",
+    description:
+      "Two friends who escaped from a juvenile work farm take Emmett Watson on an unexpected journey to New York City in 1954.",
+    amazon_link: "https://www.amazon.com/dp/0735222355?tag=NYTBSREV-20",
+    author: "Amor Towles",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780735222359.jpg",
+  },
+  {
+    isbn: "9780593356159",
+    title: "THE MAID",
+    description:
+      "When a wealthy man is found dead in his room, a maid at the Regency Grand Hotel becomes a lead suspect.",
+    amazon_link: "https://www.amazon.com/dp/0593356152?tag=NYTBSREV-20",
+    author: "Nita Prose",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780593356159.jpg",
+  },
+  {
+    isbn: "9781250274618",
+    title: "THE CHRISTIE AFFAIR",
+    description:
+      "Miss Nan O’Dea becomes the mistress of Agatha Christie’s husband.",
+    amazon_link: "https://www.amazon.com/dp/1250274613?tag=NYTBSREV-20",
+    author: "Nina de Gramont",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9781250274618.jpg",
+  },
+  {
+    isbn: "9780525618584",
+    title: "CITY OF THE DEAD",
+    description:
+      "The 37th book in the Alex Delaware series. Delaware and Sturgis investigate a double homicide.",
+    amazon_link: "https://www.amazon.com/dp/0525618589?tag=NYTBSREV-20",
+    author: "Jonathan Kellerman",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780525618584.jpg",
+  },
+  {
+    isbn: "9780385546027",
+    title: "THE JUDGE'S LIST",
+    description:
+      "The second book in the Whistler series. Investigator Lacy Stoltz goes after a serial killer and closes in on a sitting judge.",
+    amazon_link: "https://www.amazon.com/dp/0385546025?tag=NYTBSREV-20",
+    author: "John Grisham",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780385546027.jpg",
+  },
+  {
+    isbn: "9780316499774",
+    title: "THE HORSEWOMAN",
+    description:
+      "As the Paris Olympics draw near, a mother and daughter, who are champion horse riders, compete against each other.",
+    amazon_link: "https://www.amazon.com/dp/0316499773?tag=NYTBSREV-20",
+    author: "James Patterson and Mike Lupica",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780316499774.jpg",
+  },
+  {
+    isbn: "9781984818416",
+    title: "WISH YOU WERE HERE",
+    description:
+      "Diana O’Toole re-evaluates her seemingly perfect life when a pandemic disrupts her vacation in the Galápagos Islands.",
+    amazon_link: "https://www.amazon.com/dp/1984818414?tag=NYTBSREV-20",
+    author: "Jodi Picoult",
+    recommendedBy: "Karrar Qasim",
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9781984818416.jpg",
+  },
+];
 
-    module.exports = books
+module.exports = books;
