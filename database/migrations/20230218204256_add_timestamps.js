e916dc22be69c3aa9171d8b9898ea586a69ef50e
Reid Poole <poole.reid@gmail.com>
2023-02-18 15:17:58 -0600

    add database file

---
+++ database/migrations/20230218204256_add_timestamps.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.table("books", (table) => {
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table("books", (table) => {
+    table.dropTimestamps();
+  });
+};
