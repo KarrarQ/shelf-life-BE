eb964963e6738fde68b76e6e04a4ac672380961f
John Ammon <ammonj78@gmail.com>
2023-02-20 19:23:38 -0700

    Update server

--- server.js
+++ server.js
@@ -20,21 +20,24 @@ app.get("/", (request, response) => {
 
 
 app.get("/api/v1/books", (request, response) => {
-    queries.getAll().then((data) => response.send(data));
-    //   const { books } = app.locals;
-    //   response.status(200).json({ books });
+    queries.getAllBooks()
+        .then((data) => response.status(200).json(data))
+        .catch(error => response.status(500).json({ error }))///double check the error in json
 });
 
 app.get("/api/v1/books/:isbn", (request, response) => {
-    const { isbn } = request.params;
-    const book = app.locals.books.find((book) => book.isbn === isbn);
-    if (!book) {
-        return response.status(404).json({
-            message: `no novel found with isbn of # ${isbn}`,
-        });
-    } else {
-        response.status(200).json(book);
-    }
+    queries.getSingleBook(request)
+    .then(books => {
+        if(books.length){
+             response.status(200).json(books)
+        }
+        else{
+             response.status(404).json({
+                error: `no novel found with isbn of # ${request.params.isbn}`,
+            });
+        }
+    })
+    .catch(error => response.status(500).json({ error }));
 });
 
 app.post("/api/vi/favorites", (request, response) => {
