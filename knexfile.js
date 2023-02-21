9db3f1d4d8cf516246ae2c1f45938d7cd3afa039
Reid Poole <poole.reid@gmail.com>
2023-02-20 23:30:27 -0600

    Uncomment Port for successful post to PG ADMIN

--- knexfile.js
+++ knexfile.js
@@ -5,23 +5,23 @@ module.exports = {
     client: "postgresql",
     connection: {
       database: "shelf_life",
-      // port: 5432, //maybe not necassary
+      port: 5432, //maybe not necassary
       user: "postgres",
-      password: "RLft353244"
+      password: "RLft353244",
     },
     migrations: {
-      directory: "./migrations"
+      directory: "./migrations",
       // tableName: "knex_migrations",
     },
     seeds: {
-      directory: "./seeds"
-    }
+      directory: "./seeds",
+    },
   },
   production: {
     client: "postgresql",
     connection: {
       connectionString: process.env.DATABASE_URL, //may want to check this if breaking
-      ssl: { rejectUnauthorized: false }
-    }
-  }
+      ssl: { rejectUnauthorized: false },
+    },
+  },
 };
