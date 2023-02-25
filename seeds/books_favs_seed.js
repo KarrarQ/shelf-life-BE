7fe4e1b65db6ae2bbbeff382b11f44dca00e992a
John Ammon <ammonj78@gmail.com>
2023-02-24 19:16:41 -0700

    Seeded top 100 data

--- seeds/books_favs_seed.js
+++ seeds/books_favs_seed.js
@@ -1,5 +1,6 @@
 const booksData = require("../data/books-data");
 const favorites = require("../data/favorites-data");
+const top100 = require("../data/top100-data")
 
 exports.seed = function(knex) {
   return knex("books")
@@ -15,6 +16,17 @@ exports.seed = function(knex) {
         .del()
         .then(() => {
           return knex.raw("ALTER SEQUENCE favorites_id_seq RESTART WITH 1");
+        })
+        .then(() => {
+          return knex("top100")
+        })
+        .del()
+        .then(() => {
+          return knex.raw("ALTER SEQUENCE top100_id_seq RESTART WITH 1")
+        })
+        .then(() => {
+          return knex("top100").insert(top100)
+
         })
         // .then(() => {
         //   return knex("favorites").insert(favorites); //if favorites are autoIncrementing then remove last ".then"
